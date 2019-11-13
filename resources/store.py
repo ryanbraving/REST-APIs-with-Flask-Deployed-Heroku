079787b2356a0fd3a680a4b6a5a77efc39522749
Ryan Braving <ryan.braving@gmail.com>
2019-11-13 20:27:00 +1300

    a typo got fixed

--- resources/store.py
+++ resources/store.py
@@ -1,23 +1,24 @@
 from flask_restful import Resource
 from models.store import StoreModel
 
+
 class Store(Resource):
     def get(self, name):
         store = StoreModel.find_by_name(name)
         if store:
-            return store.jason(), 200
+            return store.json(), 200
         return {'message': 'Store not found'}, 404
 
     def post(self, name):
         if StoreModel.find_by_name(name):
             return {'message': 'A store with name: {} already exist'.format(name)}, 400
-        
+
         store = StoreModel(name)
         try:
             store.save_to_db()
         except:
-            return {'message': 'An error ccured while creating the store'} , 500
-        
+            return {'message': 'An error ccured while creating the store'}, 500
+
         return store.json(), 201
 
     def delete(self, name):
